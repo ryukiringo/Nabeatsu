c0a59fa076bf00bc27e22b39fd9112e2cdd16799
Ryuki <coderryuki@gmail.com>
2021-05-12 01:10:35 +0900

    Added some additional function.

--- Nabeatsu/ViewController.swift
+++ Nabeatsu/ViewController.swift
@@ -19,43 +19,68 @@ class ViewController: UIViewController {
         
     }
     
-    func isAho() -> Bool {
-        var checkNum: Int = number
-        
-        if (number % 3) == 0 {
-            return true
-        }
-        
-        while checkNum != 0 {
-            if (checkNum % 10) == 3 {
-                return true
-            } else {
-                checkNum /= 10
-            }
-        }
-
-        
-        return false
-    }
     
     @IBAction func plusButton(){
         number = number + 1
         countLabel.text = String(number)
-        
-        if isAho() == true {
-            
-            faceLabel.text = "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
-        } else {
-            
-            faceLabel.text = "(゜o゜)"
-        }
+        isAho()
     }
     
     @IBAction func clearButton() {
         number = 0
         countLabel.text = String(number)
-        faceLabel.text == "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
+        isAho()
+    }
+    
+    @IBAction func random() {
+        number = Int.random(in: 0...100)
+        isAho ()
     }
 
+    func isAho() {
+        var checkNum: Int = number
+        
+        if number == 0 {
+            faceLabel.text = "(゜o゜)"
+        } else if (number % 5) == 0 {
+            faceLabel.text = "(∪＾ω＾)ゴ！"
+        } else if (number % 7) == 0 {
+            faceLabel.text = "(゜o゜)"
+        } else if (number % 3) == 0 {
+            faceLabel.text = "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
+        } else if number != 0 {
+            while checkNum != 0 {
+                if (checkNum % 10) == 3 {
+                    faceLabel.text = "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
+                    break
+                } else {
+                    checkNum /= 10
+                }
+                if primeNumberCheck() == true {
+                    faceLabel.text = "(　･`ω･´)ｷﾘｯ 素数!!"
+                } else {
+                    faceLabel.text = "(゜o゜)"
+                }
+            }
+        }
+    }
+   
+    func primeNumberCheck() -> Bool {
+        var numberForCheck: Int = 2
+        if number == 1 {
+            return true
+        } else {
+            while (number % numberForCheck) != 0 {
+                numberForCheck += 1
+            }
+            
+            if numberForCheck ==  number {
+                return false
+            } else  {
+                return true
+            }
+        }
+    }
+    
 }
 
